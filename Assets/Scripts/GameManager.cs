b5a13f0b2bf5a58f63c74a6e453150e2c81ec86c
Noe Vazquez <famvazpom@gmail.com>
2020-04-21 02:01:07 -0500

    Completed Character Selection
    Choose Character completed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject respawn;
+    public GameObject malePlayer;
+    public GameObject femalePlayer;
+    private string selection;
+    
+    void Start()
+    {
+        selection = PlayerPrefs.GetString("Seleccion");
+        respawn = GameObject.FindGameObjectWithTag("Respawn");
+        
+        CreatePlayer();
+    }
+
+    private void CreatePlayer()
+    {
+        switch(selection)
+        {
+            case "Male":
+                Instantiate(malePlayer,respawn.transform);
+                break;
+
+             case "Female":
+                Instantiate(femalePlayer,respawn.transform);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
