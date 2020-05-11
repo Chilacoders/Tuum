682a382ed524277a1c6daec4df6b8fc257b4e504
Noe Vazquez <famvazpom@gmail.com>
2020-05-11 18:44:49 -0500

    Created Floating Text

---
+++ Assets/Scripts/floatingText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatingText : MonoBehaviour
+{
+    public string sortingLayer = "Textos";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().sortingLayerName = sortingLayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/textManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textManager : MonoBehaviour
+{
+    public GameObject[] texts;
+    public static textManager instance;
+    private int textqty; // Quantity of texts
+    
+    void OnEnable() {
+        instance = this;
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        texts = GameObject.FindGameObjectsWithTag("text");
+        HideText();
+    }
+
+    private void HideText()
+    {
+        foreach (GameObject item in texts)
+        {
+            item.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
